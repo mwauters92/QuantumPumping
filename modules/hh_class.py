49c9990c07fdab4eee3b1cc19d7582ace4c3d966
Matteo Wauters <matteo.wauters@gmail.com>
2023-06-15 10:40:53 +0200

    Added the average induced charge as an extra tunable model parameter Added functions tot_number, Hsc_Josephson, get_bond_current to the hhm class

--- modules/hh_class.py
+++ modules/hh_class.py
@@ -83,8 +83,7 @@ class hhm:
         self.dt = ( self.tf - self.t0 ) / (n_t * n_p) 
 
         self.time_domain = np.linspace( self.t0, self.tf, self.n_t * n_p) 
-        #tvec = np.linspace(0,  num_periods* 2*np.pi/omega,  num_periods*Nt+1)
-        
+                
         if return_domain:
             return self.time_domain
 
@@ -103,16 +102,25 @@ class hhm:
         np.random.seed(seed)
 
 
-    def set_Htpars(self, El, phase, delta_n=0.5, Ej0=0.5, Ec0=1.0,  
+    def set_Htpars(self, El, phase, delta_n=None, n_ave=0.5, Ej0=1.0, Ec0=4.0,  
                    noise_Ej=0.0, noise_Ec=0.0, Mcut=2, return_pars=False):
 
         L = self.system_size 
-        j = np.arange(L)
-        self.ng_t = lambda ph: delta_n*np.cos(ph - 2*np.pi/3*j) 
+         
 
         self.phase, self.El = phase, El
         self.Ec0, self.Ej0 = Ec0, Ej0
-        self.delta_n = delta_n 
+        
+        if delta_n is None:
+            self.delta_n = 0.5*Ej0/Ec0
+        else:
+            self.delta_n = delta_n
+        
+        j = np.arange(L)
+        self.ng_t = lambda ph: delta_n*np.cos(ph - 2*np.pi/3*j)
+        
+        self.n_ave=n_ave
+        
         self.Mcut = Mcut  
 
         self.Ej = Ej0 + (np.random.rand(L-1)-0.5)*noise_Ej
@@ -121,7 +129,8 @@ class hhm:
         self.noise_Ej = noise_Ej
         self.noise_Ec = noise_Ec
         
-        self.Hsc_J = Hsc_Josephson(self.system_size,self.Ej,self.El,self.phase, self.Mcut, self.pbc)
+        self.Hsc_J = self.Hsc_Josephson(self.system_size, self.Ej, self.El, self.phase, self.Mcut, self.pbc)
+        
         error_msg = ('Hsc_J is not a Hermitian operator. You may want to \
                      reconsider what you are doing.')
         assert self.Hsc_J.isherm, error_msg
@@ -134,34 +143,24 @@ class hhm:
         The function is written so that it is callable by qutip.mesolve()
         Parameters:
             t (float): time
-            args: dictionary with Hamiltonian parameters
-                'L': number of sites
-                'delta_n':amplitude of the driving of the induced charge
-                'Ec': charging energy of each island
-                'Mcut': truncation of the local Hilbert space
-                'omega': frequency of the driving.
         
         Returns:
             H+Hsc_J: total time-dependent Hamiltonian
         '''
        
-        L = self.system_size
-        omega = self.omega
-        delta_n = self.delta_n
-        Ec, Ej = self.Ec, self.Ej
-        Mcut = self.Mcut 
+        L = self.system_size      
         
-        Ec = expand_variable( Ec, L )
-        ng = self.ng_t( omega * t ) 
-        op_list = [qeye(Mcut) for j in range(L)]
+        ng = self.n_ave + self.ng_t( self.omega * t ) 
+        
+        op_list = [qeye(self.Mcut) for j in range(L)]
         
         H = Qobj( dims=tensor(op_list).dims )
 
         for j in range(L):
 
-            op_list[j] = Ec[j] * ( num(Mcut) - ng[j] )**2
+            op_list[j] = self.Ec[j] * ( num(self.Mcut) - ng[j] )**2
             H += tensor(op_list)
-            op_list[j] = qeye(Mcut)
+            op_list[j] = qeye(self.Mcut)
         
         return H + self.Hsc_J
 
@@ -179,17 +178,15 @@ class hhm:
 
         """
 
-        L = self.system_size
-        delta_n = self.delta_n
-        Mcut = self.Mcut
-        PBC = self.pbc
         
-        ng = 0.5 + self.ng_t(0)
-        Hsc = Hsc_chain_static(L, ng, self.Ec, self.Ej, 0, 0, Mcut=Mcut, PBC=PBC) 
+        
+        ng = self.n_ave + self.ng_t(0)
+        Hsc = self.hamiltonian(0) 
+        #Hsc_chain_static(L, ng, self.Ec, self.Ej, 0, 0, Mcut=Mcut, PBC=PBC) 
         _, states = Hsc.eigenstates()
 
         # pick lowest-energy configuration 
-        gs = ket2dm( states[0] )
+        gs = states[0]
         return gs #states[0]
 
 
@@ -212,13 +209,10 @@ class hhm:
         """
 
         L = self.system_size 
-        Mcut = self.Mcut 
-        Ej, El = self.Ej, self.El
-        phase = self.phase
-        
-        tot_current, current_density = get_bond_current(L, Mcut, Ej, El, 
-                                                        phase, PBC=self.pbc)
-        tot_num = total_number(L, Mcut, density=True)
+                
+        tot_current, current_density = self.get_bond_current(L, self.Mcut, self.Ej, self.El, 
+                                                        self.phase, PBC=self.pbc)
+        tot_num = self.total_number(L, self.Mcut, density=True)
 
         #op_dict = {'tot_num': tot_num, 'current': tot_current}
         #op_dict = {'tot_num': tot_num, 'current': current_density}
@@ -228,10 +222,138 @@ class hhm:
         return op_dict 
 
  
+
+    def total_number(self,L, Mcut, density=False):
+        '''
+        defines the number operator for a chain of bosonic sites
+        Parameters:
+            L (int): length of the chain (sites, not unit cells)
+            Mcut (int): truncation of the local Hilbert space. Mcut=2 for hard core bosons
+            density (bool): default False. If true it returns the local density operator instead of the number operator
+
+        Returns:
+            qutip.Qobj(dims=[Mcut ^ L]), either the total number operator N=sum_j n_j or the space resolved local
+            boson number n_j
+
+        '''
+        op_list=[qeye(Mcut) for j in range(L)]
+        num_tot = Qobj(dims=tensor(op_list).dims)
+        d_op=[]
+        for j in range(L):
+            op_list[j] = num(Mcut)
+            num_tot += tensor(op_list)
+            d_op.append(tensor(op_list))
+            op_list[j] = qeye(Mcut)
+
+        if density:
+            return d_op
+        else:
+            return num_tot
+
+
+    def get_bond_current(self,L,Mcut, Ej, El, phlead, PBC=False):
+        '''
+        Defines the current operator for a chain of bosonic sites
+        Parameters:
+            L (int): number of bosonic sites
+            Mcut (int): truncation of the local Hilbert space
+            Ej (float): Josephson coupling between neighbouring sites. Can be either a number or an array of size L
+            El (float): Josephson coupling between leads and the outer SC islands.
+            phlead (fload): the phase difference between the left and right SC leads.
+            PBC (bool): default False. If True, the system has periodic boundaries
+
+        Returns:
+            Jcurrent: average current operator 
+            current_density: list with the current density on each link of the chain
+
+        '''
+
+        op_list=[qeye(Mcut) for j in range(L)]
+
+        Jcurrent=Qobj(dims=tensor(op_list).dims)
+        current_density=[]    
+        Ej=expand_variable(Ej,L-1)
+
+        for j in range(L-1):
+            op_list[j]=create(Mcut)
+            op_list[j+1]=destroy(Mcut)
+            op_local_jump = -0.5j*Ej[j]*tensor(op_list)
+            Jcurrent+= (op_local_jump+op_local_jump.dag())
+            current_density.append(op_local_jump+op_local_jump.dag())
+            op_list[j] = qeye(Mcut) 
+
+        if PBC:
+            op_list=[qeye(Mcut) for j in range(L)]
+            op_list[-1] = create(Mcut)
+            op_list[0]=destroy(Mcut)
+            op_local_jump = -0.5j*Ej[0]*tensor(op_list)
+            Jcurrent+= (op_local_jump+op_local_jump.dag())
+            current_density.append(op_local_jump+op_local_jump.dag())
+            links=L
+        else: 
+            op_list=[qeye(Mcut) for j in range(L)]
+            op_list[0]=destroy(Mcut)
+            op_jump_left = -0.5j*np.exp(-1j*phlead)*El*tensor(op_list) # CHECK signs!!
+            Jcurrent+= (op_jump_left+op_jump_left.dag())
+            op_list[0]=qeye(Mcut)
+            op_list[-1]=create(Mcut)
+            op_jump_right=-0.5j*El*tensor(op_list)
+            Jcurrent+=(op_jump_right+op_jump_right.dag())
+            links=L+1
+        return Jcurrent/links, current_density
+
+
+
+
+
+    def Hsc_Josephson(self,L,Ej,El,phlead, Mcut=2, PBC=False):
+        '''
+        Defines the static part of the HH Hamiltonian, i.e. the Josephson couplings.
+        Parameters:
+            L (int): number of sites
+            Ej (float): tunnelling amplitude. Can be either a number or an array of size L
+            El (float): Josephson coupling between leads and the outer SC islands.
+            phlead (float): the phase difference between the left and right SC leads.
+            Mcut (int): default 2, truncation of the local Hilbert space.
+            PBC (bool): default False. If True, the system has PBC
+
+        Returns:
+            H: hopping Hamiltonian
+        '''
+        Ej=expand_variable(Ej,L-1)
+
+        op_list=[qeye(Mcut) for j in range(L)]
+
+        H=Qobj(dims=tensor(op_list).dims)
+
+        for j in range(L-1):
+            op_list[j]=create(Mcut)
+            op_list[j+1]=destroy(Mcut)
+            op_local_jump = -0.5*Ej[j]*tensor(op_list)
+            H+= (op_local_jump+op_local_jump.dag())
+            op_list[j] = qeye(Mcut)
+
+        if PBC:
+            op_list=[qeye(Mcut) for j in range(L)]
+            op_list[-1] = create(Mcut)
+            op_list[0]=destroy(Mcut)
+            op_local_jump = -0.5*Ej[j]*tensor(op_list)
+            H+= (op_local_jump+op_local_jump.dag())
+        else:
+            op_list=[qeye(Mcut) for j in range(L)]
+            op_list[0]=destroy(Mcut)
+            op_jump_left = -0.5*El*np.exp(-1j*phlead)*tensor(op_list)
+            H+= (op_jump_left+op_jump_left.dag())
+            op_list[0]=qeye(Mcut)
+            op_list[-1]=create(Mcut)
+            op_jump_right = -0.5*El*tensor(op_list)
+            H+=(op_jump_right+op_jump_right.dag())
+
+        return H
+
 #------------------------------------------------------------------------------
 # end of hh class
 #------------------------------------------------------------------------------
-
 def expand_variable(x,L):
     '''
     utility function: if x is an array or a list it does nothing, if x is a number it creates an array of 
@@ -240,86 +362,6 @@ def expand_variable(x,L):
     if np.size(x)==1:
         x = np.ones(L)*x
     return x
-
-
-def total_number(L, Mcut, density=False):
-    '''
-    defines the number operator for a chain of bosonic sites
-    Parameters:
-        L (int): length of the chain (sites, not unit cells)
-        Mcut (int): truncation of the local Hilbert space. Mcut=2 for hard core bosons
-        density (bool): default False. If true it returns the local density operator instead of the number operator
-        
-    Returns:
-        qutip.Qobj(dims=[Mcut ^ L]), either the total number operator N=sum_j n_j or the space resolved local
-        boson number n_j
-        
-    '''
-    op_list=[qeye(Mcut) for j in range(L)]
-    num_tot = Qobj(dims=tensor(op_list).dims)
-    d_op=[]
-    for j in range(L):
-        op_list[j] = num(Mcut)
-        num_tot += tensor(op_list)
-        d_op.append(tensor(op_list))
-        op_list[j] = qeye(Mcut)
-
-    if density:
-        return d_op
-    else:
-        return num_tot
-    
-    
-def get_bond_current(L,Mcut, Ej, El, phlead, PBC=False):
-    '''
-    Defines the current operator for a chain of bosonic sites
-    Parameters:
-        L (int): number of bosonic sites
-        Mcut (int): truncation of the local Hilbert space
-        Ej (float): Josephson coupling between neighbouring sites. Can be either a number or an array of size L
-        El (float): Josephson coupling between leads and the outer SC islands.
-        phlead (fload): the phase difference between the left and right SC leads.
-        PBC (bool): default False. If True, the system has periodic boundaries
-    
-    Returns:
-        Jcurrent: average current operator 
-        current_density: list with the current density on each link of the chain
-    
-    '''
-    
-    op_list=[qeye(Mcut) for j in range(L)]
-    
-    Jcurrent=Qobj(dims=tensor(op_list).dims)
-    current_density=[]    
-    Ej=expand_variable(Ej,L-1)
-    
-    for j in range(L-1):
-        op_list[j]=create(Mcut)
-        op_list[j+1]=destroy(Mcut)
-        op_local_jump = -0.5j*Ej[j]*tensor(op_list)
-        Jcurrent+= (op_local_jump+op_local_jump.dag())
-        current_density.append(op_local_jump+op_local_jump.dag())
-        op_list[j] = qeye(Mcut) 
-    
-    if PBC:
-        op_list=[qeye(Mcut) for j in range(L)]
-        op_list[-1] = create(Mcut)
-        op_list[0]=destroy(Mcut)
-        op_local_jump = -0.5j*Ej[0]*tensor(op_list)
-        Jcurrent+= (op_local_jump+op_local_jump.dag())
-        current_density.append(op_local_jump+op_local_jump.dag())
-        links=L
-    else: 
-        op_list=[qeye(Mcut) for j in range(L)]
-        op_list[0]=destroy(Mcut)
-        op_jump_left = -0.5j*np.exp(-1j*phlead)*El*tensor(op_list) # CHECK signs!!
-        Jcurrent+= (op_jump_left+op_jump_left.dag())
-        op_list[0]=qeye(Mcut)
-        op_list[-1]=create(Mcut)
-        op_jump_right=-0.5j*El*tensor(op_list)
-        Jcurrent+=(op_jump_right+op_jump_right.dag())
-        links=L+1
-    return Jcurrent/links, current_density
 
 
 def Hsc_chain_static(L, ng, Ec, Ej,El, phlead,  Mcut=2, return_dimension_only=False, PBC=False):
@@ -383,53 +425,6 @@ def Hsc_chain_static(L, ng, Ec, Ej,El, phlead,  Mcut=2, return_dimension_only=Fa
         H+=(op_jump_right+op_jump_right.dag())
         
     return H
-
-
-def Hsc_Josephson(L,Ej,El,phlead, Mcut=2, PBC=False):
-    '''
-    Defines the static part of the HH Hamiltonian, i.e. the Josephson couplings.
-    Parameters:
-        L (int): number of sites
-        Ej (float): tunnelling amplitude. Can be either a number or an array of size L
-        El (float): Josephson coupling between leads and the outer SC islands.
-        phlead (float): the phase difference between the left and right SC leads.
-        Mcut (int): default 2, truncation of the local Hilbert space.
-        PBC (bool): default False. If True, the system has PBC
-        
-    Returns:
-        H: hopping Hamiltonian
-    '''
-    Ej=expand_variable(Ej,L-1)
-    
-    op_list=[qeye(Mcut) for j in range(L)]
-    
-    H=Qobj(dims=tensor(op_list).dims)
-        
-    for j in range(L-1):
-        op_list[j]=create(Mcut)
-        op_list[j+1]=destroy(Mcut)
-        op_local_jump = -0.5*Ej[j]*tensor(op_list)
-        H+= (op_local_jump+op_local_jump.dag())
-        op_list[j] = qeye(Mcut)
-    
-    if PBC:
-        op_list=[qeye(Mcut) for j in range(L)]
-        op_list[-1] = create(Mcut)
-        op_list[0]=destroy(Mcut)
-        op_local_jump = -0.5*Ej[j]*tensor(op_list)
-        H+= (op_local_jump+op_local_jump.dag())
-    else:
-        op_list=[qeye(Mcut) for j in range(L)]
-        op_list[0]=destroy(Mcut)
-        op_jump_left = -0.5*El*np.exp(-1j*phlead)*tensor(op_list)
-        H+= (op_jump_left+op_jump_left.dag())
-        op_list[0]=qeye(Mcut)
-        op_list[-1]=create(Mcut)
-        op_jump_right = -0.5*El*tensor(op_list)
-        H+=(op_jump_right+op_jump_right.dag())
-    
-    return H
-
 #------------------------------------------------------------------------------
 # end of .py file
 #------------------------------------------------------------------------------
